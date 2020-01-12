adad62108dba42bcfe6a6902ce4adb10de9d98df
Wil Neeley <william.neeley@gmail.com>
2020-01-12 10:02:19 -0800

    Added file watcher for base development of grid systems

--- index.js
+++ index.js
@@ -12,10 +12,23 @@ require("./css/index.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var FluidGrid = function FluidGrid(_ref) {
-  var someParam = _ref.someParam;
+  var gutter = _ref.gutter,
+      children = _ref.children,
+      classAdditions = _ref.classAdditions;
+
+  // Update children to pass properties to anticipated components
+  var updatedChildren = _react["default"].Children.map(children, function (child) {
+    return _react["default"].cloneElement(child, {
+      gutter: gutter
+    });
+  }); // Build class string for css grid overrides
+
+
+  var classString = "fg-container";
+  classString += gutter ? "" : " fg-no-gutter";
   return _react["default"].createElement("div", {
-    className: "FluidGrid"
-  }, "Testing fluid grid... ", someParam);
+    className: [classString].concat(classAdditions).join(" ")
+  }, updatedChildren);
 };
 
 exports.FluidGrid = FluidGrid;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import './css/index.css';
 
 export const FluidGrid = ({
-    someParam
-                          }) => {
+  gutter,
+  children,
+  classAdditions,
+}) => {
+
+    // Update children to pass properties to anticipated components
+    const updatedChildren = React.Children.map(children, child =>
+        React.cloneElement(child, {
+            gutter: gutter,
+        }),
+    );
+
+    // Build class string for css grid overrides
+    let classString = "fg-container";
+    classString += gutter ? "" : " fg-no-gutter";
 
     return (
-        <div className="FluidGrid">
-            Testing fluid grid... {someParam}
+        <div className={[classString].concat(classAdditions).join(" ")}>
+            {updatedChildren}
         </div>
     );
 };
